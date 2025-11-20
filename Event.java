9e6ccfd35d19ee2731f4b1d029cf142b28f0f405
alitanbooz <alitanbooz@gmail.com>
2025-11-20 15:50:57 +0400

    Update Event.java

--- Event.java
+++ Event.java
@@ -1,1 +1,21 @@
+public class Event{
 
+  String name;
+  int time;
+  int date;
+
+public Event (String name, int time,int date){
+  this.name = name;
+  this.date = date;
+  this.time = time;
+
+    }
+
+  @Overriide
+  publuc String toString(){
+    return "Name: "+this.name+"Date:"+this.date+"Time:"+this.time;
+  }
+}
+}
+
+  
