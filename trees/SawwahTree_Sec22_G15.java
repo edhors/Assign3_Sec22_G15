548a0417bcca678d6ba4793ef35ab149f99ab511
eduardo <horstmanneduardo100@gmail.com>
2025-11-22 14:59:22 +0400

    create: SawwahTree class which extends TreeSet

---
+++ trees/SawwahTree_Sec22_G15.java
@@ -0,0 +1,18 @@
+package trees;
+import java.util.TreeSet;
+
+import nodes.Event_Sec22_G15;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class SawwahTree_Sec22_G15 extends TreeSet<Event_Sec22_G15> {
+    
+
+    public SawwahTree_Sec22_G15() {
+        super();
+    }
+
+
+    
+}
