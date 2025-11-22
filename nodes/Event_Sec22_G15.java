351a4103adb877909030adaef64dacc3085adf14
eduardo <horstmanneduardo100@gmail.com>
2025-11-22 14:57:33 +0400

    add: compareTo method

--- nodes/Event_Sec22_G15.java
+++ nodes/Event_Sec22_G15.java
@@ -1,60 +1,94 @@
 package nodes;
-public class Event_Sec22_G15 {
+public class Event_Sec22_G15 implements Comparable<Event_Sec22_G15> {
     private String name;
     private String category;
-    private String date;
-    private String time;
     private String location;
+    private String region;
+    private String date;
+    private String startTime;
+    private String endTime;
 
-    public Event_Sec22_G15(String name, String date, String time, String location, String category, String description) {
+    public Event_Sec22_G15(String name, String category, String location, String region, String date, String startTime, String endTime) {
         this.name = name;
-        this.date = date;
-        this.time = time;
-        this.location = location;
         this.category = category;
+        this.location = location;
+        this.region = region;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
+
+    @Override
+    public int compareTo(Event_Sec22_G15 other) {
+        if (this.date.compareTo(other.date) != 0) {
+            return this.date.compareTo(other.date);
+        }
+        
+        if (this.startTime.compareTo(other.startTime) != 0) {
+            return this.startTime.compareTo(other.startTime);
+        }
+        
+        return this.name.compareTo(other.name);
+    }
+
 
     public String getName() {
         return name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public String getCategory() {
-        return category;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
-    
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
+    public String getCategory() {
+        return category;
     }
 
     public void setCategory(String category) {
         this.category = category;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
     public String toString() {
-        return "Event: " + name + " on " + date + " at " + time + " in " + location + " (" + category + ")";
+        return "Event: " + name + " (" + category + ") in " + location + " [" + region + "] on " + date + " at " + startTime + " - " + endTime;
     }
 }
