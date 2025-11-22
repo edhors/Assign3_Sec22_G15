1e6fae0df2102cf15ed697490fd84663ccfbd819
eduardo <horstmanneduardo100@gmail.com>
2025-11-22 15:52:56 +0400

    override: add method handles null dates and times

--- trees/SawwahTree_Sec22_G15.java
+++ trees/SawwahTree_Sec22_G15.java
@@ -2,7 +2,10 @@ package trees;
 import java.util.TreeSet;
 
 import nodes.Event_Sec22_G15;
-
+import exceptions.InvalidDateFormat_Sec22_G15;
+import exceptions.InvalidYear_Sec22_G15;
+import exceptions.InvalidMonth_Sec22_G15;
+import exceptions.InvalidDay_Sec22_G15;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -12,7 +15,18 @@ public class SawwahTree_Sec22_G15 extends TreeSet<Event_Sec22_G15> {
     public SawwahTree_Sec22_G15() {
         super();
     }
-
-
     
+    @Override
+    public boolean add(Event_Sec22_G15 event) {
+        if (event.getDate() == null) {
+            return false;
+        }
+        if (event.getStartTime() == null) {
+            return false;
+        }
+        if (event.getEndTime() == null) {
+            return false;
+        }
+        return super.add(event);
+    }
 }
