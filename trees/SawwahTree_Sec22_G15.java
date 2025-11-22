109b304b609ce5992135977a2c9fe51b899313b4
eduardo <horstmanneduardo100@gmail.com>
2025-11-22 17:07:23 +0400

    add: update method add: filter method add: print methods

--- trees/SawwahTree_Sec22_G15.java
+++ trees/SawwahTree_Sec22_G15.java
@@ -2,20 +2,15 @@ package trees;
 import java.util.TreeSet;
 
 import nodes.Event_Sec22_G15;
-import exceptions.InvalidDateFormat_Sec22_G15;
-import exceptions.InvalidYear_Sec22_G15;
-import exceptions.InvalidMonth_Sec22_G15;
-import exceptions.InvalidDay_Sec22_G15;
+
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class SawwahTree_Sec22_G15 extends TreeSet<Event_Sec22_G15> {
-    
-
     public SawwahTree_Sec22_G15() {
         super();
     }
     
+    //add event to the tree, validate the event
     @Override
     public boolean add(Event_Sec22_G15 event) {
         if (event.getDate() == null) {
@@ -29,4 +24,44 @@ public class SawwahTree_Sec22_G15 extends TreeSet<Event_Sec22_G15> {
         }
         return super.add(event);
     }
+
+    //update event in the tree
+    public boolean update(Event_Sec22_G15 event) {
+        if (contains(event)) {
+            remove(event);
+            add(event);
+            return true;
+        }
+        return false;
+    }
+
+    //filter events by category, region, and date
+    public ArrayList<Event_Sec22_G15> filter(String category, String region, String date) {
+        ArrayList<Event_Sec22_G15> filteredEvents = new ArrayList<>();
+        for (Event_Sec22_G15 event : this) {
+            if ((category == null || category.equals("") || event.getCategory().toLowerCase().equals(category.toLowerCase())) 
+                && (region == null || region.equals("") || event.getRegion().toLowerCase().equals(region.toLowerCase())) 
+                && (date == null || date.equals("") || event.getDate().equals(date))) {
+                filteredEvents.add(event);
+            }
+        }
+        return filteredEvents;
+    }
+
+
+    //print events in the tree, sorted by date and time in chronological order
+    //TreeSet is already sorted by the compareTo method in the Event class
+    public void printInChronologicalOrder() {
+        for (Event_Sec22_G15 event : this)
+            System.out.println(event);
+    }
+
+    //print events in the tree, sorted by date and time in reverse chronological order
+    //use descendingSet() method to get the events in reverse chronological order
+    public void printInReverseChronologicalOrder() {
+        for (Event_Sec22_G15 event : this.descendingSet())
+            System.out.println(event);
+    }
+
+
 }
